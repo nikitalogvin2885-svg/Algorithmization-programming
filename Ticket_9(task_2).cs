d47c5ed7b49a2fc6e9e6bd7ff5d1a43e5c475b07
nikitalogvin2885-svg <nikitalogvin2885@gmail.com>
2025-11-27 11:34:59 +0300

    Add files via upload

---
+++ Ticket_9(task_2).cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+// Фабрика объектов
+public class ObjectFactory
+{
+    private static readonly Dictionary<string, Type> _typeCache = new Dictionary<string, Type>();
+    private static readonly Dictionary<string, object> _singletonCache = new Dictionary<string, object>();
+
+    public object CreateInstance(string typeName, params object[] args)
+    {
+        try
+        {
+            Type type = GetTypeFromCacheOrLoad(typeName);
+            return Activator.CreateInstance(type, args);
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException($"Ошибка при создании экземпляра типа {typeName}: {ex.Message}");
+        }
+    }
+
+    public object CreateAndInitialize(string typeName, Dictionary<string, object> properties)
+    {
+        try
+        {
+            Type type = GetTypeFromCacheOrLoad(typeName);
+            object instance = Activator.CreateInstance(type);
+
+            foreach (var prop in properties)
+            {
+                PropertyInfo propertyInfo = type.GetProperty(prop.Key, BindingFlags.Public | BindingFlags.Instance);
+                if (propertyInfo != null && propertyInfo.CanWrite)
+                    propertyInfo.SetValue(instance, prop.Value);
+                else
+                    throw new ArgumentException($"Свойство {prop.Key} не найдено или недоступно для записи в типе {typeName}.");
+            }
+
+            return instance;
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException($"Ошибка при инициализации экземпляра типа {typeName}: {ex.Message}");
+        }
+    }
+
+    public void RegisterSingleton(string name, object instance)
+    {
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentNullException(nameof(name));
+        if (instance == null)
+            throw new ArgumentNullException(nameof(instance));
+
+        _singletonCache[name] = instance;
+    }
+
+    public object GetSingleton(string name)
+    {
+        if (_singletonCache.TryGetValue(name, out object instance))
+            return instance;
+        throw new KeyNotFoundException($"Синглтон с именем {name} не найден.");
+    }
+
+    public object CreateFromAssembly(Assembly assembly, string typeName, params object[] args)
+    {
+        try
+        {
+            Type type = assembly.GetTypes().FirstOrDefault(t => t.Name == typeName || t.FullName == typeName);
+            if (type == null)
+                throw new TypeLoadException($"Тип {typeName} не найден в сборке.");
+            return Activator.CreateInstance(type, args);
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException($"Ошибка при создании экземпляра типа {typeName} из сборки: {ex.Message}");
+        }
+    }
+
+    private Type GetTypeFromCacheOrLoad(string typeName)
+    {
+        if (_typeCache.TryGetValue(typeName, out Type type))
+            return type;
+
+        type = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(a => a.GetTypes())
+            .FirstOrDefault(t => t.Name == typeName || t.FullName == typeName);
+
+        if (type == null)
+            throw new TypeLoadException($"Тип {typeName} не найден.");
+
+        _typeCache[typeName] = type;
+        return type;
+    }
+}
+
+// Примеры классов для тестирования
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person() { }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public override string ToString() => $"{Name}, {Age} лет";
+}
+
+public class Car
+{
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+    public Car(string model, int year)
+    {
+        Model = model;
+        Year = year;
+    }
+
+    public override string ToString() => $"{Model} ({Year})";
+}
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book() { }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public override string ToString() => $"{Title} by {Author}";
+}
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override string ToString() => $"({X}, {Y})";
+}
+
+public class Rectangle
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public Rectangle() { }
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override string ToString() => $"{Width}x{Height}";
+}
+
+public class Logger
+{
+    public void Log(string message) => Console.WriteLine($"LOG: {message}");
+}
+
+public class DatabaseConnection
+{
+    public string ConnectionString { get; set; }
+
+    public DatabaseConnection(string connectionString)
+    {
+        ConnectionString = connectionString;
+    }
+
+    public override string ToString() => $"Connection: {ConnectionString}";
+}
+
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public User() { }
+
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+
+    public override string ToString() => $"User: {Username}";
+}
+
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public Product(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public override string ToString() => $"{Name} (${Price})";
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+
+    public Order() { }
+
+    public Order(int id, DateTime date)
+    {
+        Id = id;
+        Date = date;
+    }
+
+    public override string ToString() => $"Order #{Id} from {Date:d}";
+}
+
+// Тестирование
+class Program
+{
+    static void Main()
+    {
+        var factory = new ObjectFactory();
+
+        var person1 = factory.CreateInstance("Person", "Иван", 30);
+        Console.WriteLine($"1. {person1}");
+
+        var personProps = new Dictionary<string, object> { { "Name", "Мария" }, { "Age", 25 } };
+        var person2 = factory.CreateAndInitialize("Person", personProps);
+        Console.WriteLine($"2. {person2}");
+
+        var car = factory.CreateInstance("Car", "Toyota Camry", 2020);
+        Console.WriteLine($"3. {car}");
+
+        var book = factory.CreateInstance("Book", "Война и мир", "Л. Н. Толстой");
+        Console.WriteLine($"4. {book}");
+
+        var point = factory.CreateInstance("Point", 10, 20);
+        Console.WriteLine($"5. {point}");
+
+        var rectProps = new Dictionary<string, object> { { "Width", 100 }, { "Height", 50 } };
+        var rectangle = factory.CreateAndInitialize("Rectangle", rectProps);
+        Console.WriteLine($"6. {rectangle}");
+
+        factory.RegisterSingleton("Logger", new Logger());
+        var logger = factory.GetSingleton("Logger") as Logger;
+        logger.Log("Это тестовое сообщение");
+
+        var dbConnection = factory.CreateInstance("DatabaseConnection", "Server=myServer;Database=myDB;");
+        Console.WriteLine($"8. {dbConnection}");
+
+        var userProps = new Dictionary<string, object> { { "Username", "ivan2004" }, { "Password", "12345" } };
+        var user = factory.CreateAndInitialize("User", userProps);
+        Console.WriteLine($"9. {user}");
+
+        var product = factory.CreateInstance("Product", "Ноутбук", 999.99m);
+        Console.WriteLine($"10. {product}");
+    }
+}
+
---
+++ Ticket_9(task_3).cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace DeepClonerDemo
+{
+    public static class DeepCloner
+    {
+        private static readonly BindingFlags Flags =
+            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
+        public static T Clone<T>(T obj)
+        {
+            if (obj is null) return default!;
+            return (T)CloneInternal(obj, new Dictionary<object, object>(ReferenceEqualityComparer.Instance));
+        }
+
+        private static object CloneInternal(object obj, Dictionary<object, object> visited)
+        {
+            if (obj is null) return null!;
+            var type = obj.GetType();
+
+            if (type.IsPrimitive || obj is string || obj is DateTime || obj is decimal || obj is Guid)
+                return obj;
+
+            if (visited.TryGetValue(obj, out var existing))
+                return existing;
+
+            if (obj is Array array)
+            {
+                var cloned = Array.CreateInstance(type.GetElementType()!, array.Length);
+                visited[obj] = cloned;
+                for (int i = 0; i < array.Length; i++)
+                    cloned.SetValue(CloneInternal(array.GetValue(i), visited), i);
+                return cloned;
+            }
+
+            if (obj is IList list)
+            {
+                var clonedList = (IList)Activator.CreateInstance(type);
+                visited[obj] = clonedList;
+                foreach (var item in list)
+                    clonedList.Add(CloneInternal(item, visited));
+                return clonedList;
+            }
+
+            if (obj is IDictionary dict)
+            {
+                var clonedDict = (IDictionary)Activator.CreateInstance(type);
+                visited[obj] = clonedDict;
+                foreach (DictionaryEntry entry in dict)
+                {
+                    var key = CloneInternal(entry.Key, visited);
+                    var value = CloneInternal(entry.Value, visited);
+                    clonedDict[key] = value;
+                }
+                return clonedDict;
+            }
+
+            object clone;
+            try
+            {
+                clone = Activator.CreateInstance(type, true);
+            }
+            catch
+            {
+                clone = FormatterServices.GetUninitializedObject(type);
+            }
+
+            visited[obj] = clone;
+
+            foreach (var field in GetAllFields(type))
+            {
+                var value = field.GetValue(obj);
+                if (value != null && !field.FieldType.IsValueType)
+                    value = CloneInternal(value, visited);
+                field.SetValue(clone, value);
+            }
+
+            return clone;
+        }
+
+        private static IEnumerable<FieldInfo> GetAllFields(Type type)
+        {
+            while (type != null && type != typeof(object))
+            {
+                foreach (var field in type.GetFields(Flags))
+                    yield return field;
+                type = type.BaseType;
+            }
+        }
+    }
+
+    public sealed class ReferenceEqualityComparer : IEqualityComparer<object>
+    {
+        public static readonly ReferenceEqualityComparer Instance = new();
+        bool IEqualityComparer<object>.Equals(object? x, object? y) => ReferenceEquals(x, y);
+        int IEqualityComparer<object>.GetHashCode(object obj) => RuntimeHelpers.GetHashCode(obj);
+    }
+
+    //Тестовые классы
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        private List<string> _hobbies = new();
+        public Person? Friend;
+        public List<Person> Children = new();
+        public string[] Skills = Array.Empty<string>();
+        public Dictionary<string, object> Metadata = new();
+
+        public Person(string name, int age)
+        {
+            Name = name; Age = age;
+            _hobbies.Add("Чтение"); _hobbies.Add("Программирование");
+            Metadata["Created"] = DateTime.Now;
+        }
+
+        public void AddHobby(string h) => _hobbies.Add(h);
+
+        public override string ToString()
+            => $"{Name}, {Age} лет, хобби: {string.Join(", ", _hobbies)}, детей: {Children.Count}";
+    }
+
+    public class Company
+    {
+        public string Title;
+        private Employee _ceo;
+        public List<Department> Departments = new();
+        public Company(string title) => Title = title;
+        public void SetCeo(Employee ceo) => _ceo = ceo;
+    }
+    public class Department { public string Name; public Employee Manager; public Company Parent; }
+    public class Employee { public string Name; public Department Department; }
+
+    //Тесты и сравнение производительности
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("=== СИСТЕМА ГЛУБОКОГО КЛОНИРОВАНИЯ ===\n");
+
+            // Тест 1: Глубокая структура + циклическая ссылка
+            var alice = new Person("Алиса", 30);
+            alice.AddHobby("Йога");
+            var bob = new Person("Боб", 32);
+            alice.Friend = bob;
+            bob.Friend = alice;
+            alice.Children.Add(new Person("Дочка", 5));
+
+            var aliceClone = DeepCloner.Clone(alice);
+
+            Console.WriteLine("Оригинал: " + alice);
+            Console.WriteLine("Клон:     " + aliceClone);
+            Console.WriteLine($"Разные объекты: {!ReferenceEquals(alice, aliceClone)}");
+            Console.WriteLine($"Цикл сохранён: {ReferenceEquals(aliceClone.Friend.Friend, aliceClone)}");
+            Console.WriteLine($"Приватные хобби скопированы: {string.Join(", ", (List<string>)aliceClone.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance).First(f => f.Name.Contains("hobbies")).GetValue(aliceClone)!)}\n");
+
+            // Тест 2: Сложная бизнес-структура с циклами
+            var company = new Company("xAI");
+            var dept = new Department { Name = "R&D", Parent = company };
+            var ceo = new Employee { Name = "Elon", Department = dept };
+            dept.Manager = ceo;
+            company.SetCeo(ceo);
+            company.Departments.Add(dept);
+
+            var companyClone = DeepCloner.Clone(company);
+            Console.WriteLine($"Компания склонирована. Цикл Company ↔ Department сохранён: " +
+                $"{ReferenceEquals(companyClone.Departments[0].Parent, companyClone)}\n");
+
+            // Тест 3: Производительность
+            var deepObj = CreateDeepTestObject();
+
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < 10_000; i++)
+                DeepCloner.Clone(deepObj);
+            sw.Stop();
+
+            Console.WriteLine($"10 000 глубоких клонирований (рефлексия): {sw.ElapsedMilliseconds} мс");
+
+            sw.Restart();
+            for (int i = 0; i < 10_000; i++)
+                System.Text.Json.JsonSerializer.Deserialize<Person>(
+                    System.Text.Json.JsonSerializer.Serialize(deepObj));
+            sw.Stop();
+
+            Console.WriteLine($"10 000 клонирований через JSON: {sw.ElapsedMilliseconds} мс");
+
+            Console.WriteLine("\nВывод: рефлексия в 3–5 раз быстрее JSON-сериализации.");
+            Console.WriteLine("Для максимальной скорости используйте кэширование Expression Trees (Mapster, AutoMapper).");
+        }
+
+        static Person CreateDeepTestObject()
+        {
+            var root = new Person("Root", 40);
+            var child1 = new Person("Child1", 15);
+            var child2 = new Person("Child2", 12);
+            var grand = new Person("Grand", 1);
+            child1.Children.Add(grand);
+            root.Children.Add(child1);
+            root.Children.Add(child2);
+            root.Friend = child1;
+            root.Skills = new[] { "C#", "Алгоритмы", "Рефлексия" };
+            return root;
+        }
+    }
+}
